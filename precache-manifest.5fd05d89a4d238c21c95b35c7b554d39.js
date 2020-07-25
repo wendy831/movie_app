9989c5cb3904da6f0b2ac3469c6ebfb6911de3b0
wendy831 <wendy831@naver.com>
2020-07-25 11:20:24 +0900

    Updates

---
+++ precache-manifest.5fd05d89a4d238c21c95b35c7b554d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aeb96c1fc9f1b00bf46f0fee5818340",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "91aa79ed3048cc48f1fc",
+    "url": "/movie_app/static/css/main.da428a20.chunk.css"
+  },
+  {
+    "revision": "dd66ef146ce11bba6924",
+    "url": "/movie_app/static/js/2.019ccfbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.019ccfbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91aa79ed3048cc48f1fc",
+    "url": "/movie_app/static/js/main.2cb0cb99.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
